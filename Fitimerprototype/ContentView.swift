d7b21ab1664f5db54fc8bddc8146ae460b604fbb
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2024-12-09 17:06:51 +0800

    Initial Commit

---
+++ Fitimerprototype/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fitimerprototype
+//
+//  Created by Amanda Cook on 9/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fitimerprototype/FitimerprototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitimerprototypeApp.swift
+//  Fitimerprototype
+//
+//  Created by Amanda Cook on 9/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct FitimerprototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
